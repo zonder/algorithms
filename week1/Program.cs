c331e0909317b5402af2ff7a38c5c083aee00df2
zontik.m@gmail.com <zontik.m@gmail.com>
2018-09-18 20:35:39 +0300

    test

--- week1/Program.cs
+++ week1/Program.cs
@@ -6,7 +6,7 @@ namespace Week1
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Console.WriteLine("Hello World1!");
       Console.ReadLine();
     }
   }
