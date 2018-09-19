c674094b5a786000f3e3ed7c6ec66700f6ab309f
zontik.m@gmail.com <zontik.m@gmail.com>
2018-09-20 00:04:25 +0300

    added java examples and template for assesment

---
+++ week1/java/Percolations/src/Percolation.java
@@ -0,0 +1,71 @@
+/******************************************************************************
+ *  Compilation:  javac-algs4 Percolation.java
+ *  Execution:    java-algs4 Percolation n
+ *
+ *
+ ******************************************************************************/
+
+public class Percolation {
+  /**
+   * create n-by-n grid, with all sites blocked
+   *
+   * @param n - size of grid
+   */
+  public Percolation(int n) {
+
+  }
+
+  /**
+   * open site (row, col) if it is not open already
+   *
+   * @param row - row index of site
+   * @param col - col index of site
+   */
+  public void open(int row, int col) {
+
+  }
+
+  /**
+   * Detects if site (row, col) is open?
+   *
+   * @param row - row index of site
+   * @param col - col index of site
+   * @return site openness
+   */
+  public boolean isOpen(int row, int col) {
+    return false;
+  }
+
+  /**
+   * Detects if site (row, col) is full?
+   *
+   * @param row - row index of site
+   * @param col - col index of site
+   * @return site fullness
+   */
+  public boolean isFull(int row, int col) {
+    return false;
+  }
+
+  /**
+   * Calculates number of open sites
+   *
+   * @return number of open sites
+   */
+  public int numberOfOpenSites() {
+    return 0;
+  }
+
+  /**
+   * Checks if the system percolate?
+   *
+   * @return percolates or not
+   */
+  public boolean percolates() {
+    return false;
+  }
+
+  public static void main(String[] args) {
+    System.out.print("Percolation");
+  }
+}
---
+++ week1/java/Percolations/src/PercolationStats.java
@@ -0,0 +1,64 @@
+/******************************************************************************
+ *  Compilation:  javac-algs4 Percolation.java
+ *  Execution:    java-algs4 Percolation n
+ *
+ *
+ ******************************************************************************/
+
+public class PercolationStats {
+
+  /**
+   * perform trials independent experiments on an n-by-n grid
+   *
+   * @param n      - size of grid
+   * @param trials - count of experiments
+   */
+  public PercolationStats(int n, int trials) {
+
+  }
+
+  /**
+   * Calculates sample mean of percolation threshold
+   *
+   * @return sample mean
+   */
+  public double mean() {
+    return 0;
+
+  }
+
+  /**
+   * Calculates sample standard deviation of percolation threshold
+   *
+   * @return sample standard deviation of percolation threshold
+   */
+  public double stddev() {
+    return 0;
+  }
+
+  /**
+   * Calculates low endpoint of 95% confidence interval
+   *
+   * @return low endpoint of 95% confidence interval
+   */
+  public double confidenceLo() {
+    return 0;
+  }
+
+  /**
+   * Calculates high endpoint of 95% confidence interval
+   *
+   * @return high endpoint of 95% confidence interval
+   */
+  public double confidenceHi() {
+    return 0;
+  }
+
+  /**
+   * @param args n - number of columns and rows
+   *             T - count of experiments
+   */
+  public static void main(String[] args) {
+    System.out.print("PercolationStats");
+  }
+}
