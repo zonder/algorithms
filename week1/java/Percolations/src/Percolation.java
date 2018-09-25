10b6626141e7c24a200419a0e454043f2c57df09
Alexander Masalov <alexander.masalov@namecheap.com>
2018-09-25 14:05:58 +0300

    minor updates

--- week1/java/Percolations/src/Percolation.java
+++ week1/java/Percolations/src/Percolation.java
@@ -1,142 +1,141 @@
-import edu.princeton.cs.algs4.QuickFindUF;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /******************************************************************************
- *  Compilation:  javac-algs4 Percolation.java
- *  Execution:    java-algs4 Percolation n
+ * Compilation: javac-algs4 Percolation.java Execution: java-algs4 Percolation n
  *
  *
  ******************************************************************************/
 
 public class Percolation {
-  private QuickFindUF _unionFindAlgorithm;
-  private int _n;
-  private int _openCount = 0;
+    private WeightedQuickUnionUF _unionFindAlgorithm;
+    private int _n;
+    private int _openCount = 0;
 
-  private boolean[] _states;
+    private boolean[] _states;
 
-  /**
-   * create n-by-n grid, with all sites blocked
-   *
-   * @param n - size of grid
-   */
-  public Percolation(int n) {
-    if (n <= 0)
-      throw new java.lang.IllegalArgumentException();
+    /**
+     * create n-by-n grid, with all sites blocked
+     *
+     * @param n - size of grid
+     */
+    public Percolation(int n) {
+        if (n <= 0)
+            throw new java.lang.IllegalArgumentException();
 
-    _unionFindAlgorithm = new QuickFindUF(n * n);
-    _n = n;
-    _states = new boolean[n * n];
-  }
-
-  /**
-   * open site (row, col) if it is not open already
-   *
-   * @param row - row index of site
-   * @param col - col index of site
-   */
-  public void open(int row, int col) {
-    var rowIndex = row - 1;
-    var colIndex = col - 1;
-
-    int p = (_n * rowIndex) + colIndex;
-    if (p < 0)
-      return;
-
-    if (row > 1 && isOpen(row - 1, col)) {
-      int q = (_n * (rowIndex - 1)) + colIndex;
-      _unionFindAlgorithm.union(p, q);
-    }
-    if (row < _n && isOpen(row + 1, col)) {
-      int q = (_n * (rowIndex + 1)) + colIndex;
-      _unionFindAlgorithm.union(p, q);
+        _unionFindAlgorithm = new WeightedQuickUnionUF(n * n);
+        _n = n;
+        _states = new boolean[n * n];
     }
 
-    if (col > 1 && isOpen(row, col - 1)) {
-      int q = (_n * rowIndex) + (colIndex - 1);
-      _unionFindAlgorithm.union(p, q);
+    /**
+     * open site (row, col) if it is not open already
+     *
+     * @param row - row index of site
+     * @param col - col index of site
+     */
+    public void open(int row, int col) {
+        int rowIndex = row - 1;
+        int colIndex = col - 1;
+
+        int p = (_n * rowIndex) + colIndex;
+        if (p < 0)
+            return;
+
+        if (row > 1 && isOpen(row - 1, col)) {
+            int q = (_n * (rowIndex - 1)) + colIndex;
+            _unionFindAlgorithm.union(p, q);
+        }
+        if (row < _n && isOpen(row + 1, col)) {
+            int q = (_n * (rowIndex + 1)) + colIndex;
+            _unionFindAlgorithm.union(p, q);
+        }
+
+        if (col > 1 && isOpen(row, col - 1)) {
+            int q = (_n * rowIndex) + (colIndex - 1);
+            _unionFindAlgorithm.union(p, q);
+        }
+
+        if (col < _n && isOpen(row, col + 1)) {
+            int q = (_n * rowIndex) + (colIndex + 1);
+            _unionFindAlgorithm.union(p, q);
+        }
+
+        _states[p] = true;
+        _openCount++;
     }
 
-    if (col < _n && isOpen(row, col + 1)) {
-      int q = (_n * rowIndex) + (colIndex + 1);
-      _unionFindAlgorithm.union(p, q);
+    /**
+     * Detects if site (row, col) is open?
+     *
+     * @param row - row index of site
+     * @param col - col index of site
+     * @return site openness
+     */
+    public boolean isOpen(int row, int col) {
+        int p = (_n * (row - 1)) + (col - 1);
+        if (p < 0)
+            return false;
+        return _states[p];
     }
 
-    _states[p] = true;
-    _openCount++;
-  }
-
-  /**
-   * Detects if site (row, col) is open?
-   *
-   * @param row - row index of site
-   * @param col - col index of site
-   * @return site openness
-   */
-  public boolean isOpen(int row, int col) {
-    int p = (_n * (row - 1)) + (col - 1);
-    if (p < 0)
-      return false;
-    return _states[p];
-  }
-
-  /**
-   * Detects if site (row, col) is full?
-   *
-   * @param row - row index of site
-   * @param col - col index of site
-   * @return site fullness
-   */
-  public boolean isFull(int row, int col) {
-    int p = (_n * (row - 1)) + (col - 1);
-    for (int i = 0; i < _n; i++) {
-      if (_unionFindAlgorithm.connected(p, i))
-        return true;
+    /**
+     * Detects if site (row, col) is full?
+     *
+     * @param row - row index of site
+     * @param col - col index of site
+     * @return site fullness
+     */
+    public boolean isFull(int row, int col) {
+        int p = (_n * (row - 1)) + (col - 1);
+        for (int i = 0; i < _n; i++) {
+            if (_unionFindAlgorithm.connected(p, i))
+                return true;
+        }
+        return false;
     }
-    return false;
-  }
 
-  /**
-   * Calculates number of open sites
-   *
-   * @return number of open sites
-   */
-  public int numberOfOpenSites() {
-    return _openCount;
-  }
-
-  /**
-   * Checks if the system percolate?
-   *
-   * @return percolates or not
-   */
-  public boolean percolates() {
-    for (int i = 0; i < _n; i++) {
-      for (int j = 0; j < _n; j++)
-        if (_unionFindAlgorithm.connected((_n * _n) - j - 1, i))
-          return true;
+    /**
+     * Calculates number of open sites
+     *
+     * @return number of open sites
+     */
+    public int numberOfOpenSites() {
+        return _openCount;
     }
-    return false;
-  }
 
-  public static void main(String[] args) {
-    int n = 20;
-    var percolation = new Percolation(n);
-    boolean isPercolated = false;
-    int counter = 0;
-    do {
+    /**
+     * Checks if the system percolate?
+     *
+     * @return percolates or not
+     */
+    public boolean percolates() {
+        for (int i = 0; i < _n; i++) {
+            for (int j = 0; j < _n; j++)
+                if (_unionFindAlgorithm.connected((_n * _n) - j - 1, i))
+                    return true;
+        }
+        return false;
+    }
 
-      var row = StdRandom.uniform(1, n + 1);
-      var column = StdRandom.uniform(1, n + 1);
-      if (!percolation.isOpen(row, column)) {
-        percolation.open(row, column);
-        if (percolation.percolates())
-          isPercolated = true;
+    public static void main(String[] args) {
+        int n = 20;
+        Percolation percolation = new Percolation(n);
+        boolean isPercolated = false;
+        int counter = 0;
+        do {
 
-        counter++;
-      }
+            int row = StdRandom.uniform(1, n + 1);
+            int column = StdRandom.uniform(1, n + 1);
+            if (!percolation.isOpen(row, column)) {
+                percolation.open(row, column);
+                if (percolation.percolates())
+                    isPercolated = true;
 
-    } while (!isPercolated);
-    System.out.println(counter + "/" + n * n);
-  }
+                counter++;
+            }
+
+        } while (!isPercolated);
+        System.out.println(counter + "/" + n * n);
+    }
 }
--- week1/java/Percolations/src/PercolationStats.java
+++ week1/java/Percolations/src/PercolationStats.java
@@ -2,99 +2,95 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 /******************************************************************************
- *  Compilation:  javac-algs4 Percolation.java
- *  Execution:    java-algs4 Percolation n
+ * Compilation: javac-algs4 Percolation.java Execution: java-algs4 Percolation n
  *
  *
  ******************************************************************************/
 
 public class PercolationStats {
 
-  private int _n;
-  private int _trials;
-  private double[] _thresholds;
+    private int _trials;
+    private double[] _thresholds;
 
-  /**
-   * perform trials independent experiments on an n-by-n grid
-   *
-   * @param n      - size of grid
-   * @param trials - count of experiments
-   */
-  public PercolationStats(int n, int trials) {
+    /**
+     * perform trials independent experiments on an n-by-n grid
+     *
+     * @param n      - size of grid
+     * @param trials - count of experiments
+     */
+    public PercolationStats(int n, int trials) {
 
-    if (n <= 0 || trials <= 0)
-      throw new java.lang.IllegalArgumentException();
+        if (n <= 0 || trials <= 0)
+            throw new java.lang.IllegalArgumentException();
 
-    _n = n;
-    _trials = trials;
-    _thresholds = new double[trials];
+        _trials = trials;
+        _thresholds = new double[trials];
 
-    for (int i = 0; i < trials; i++) {
-      var percolation = new Percolation(n);
-      boolean isPercolated = false;
-      int counter = 0;
-      do {
+        for (int i = 0; i < trials; i++) {
+            Percolation percolation = new Percolation(n);
+            boolean isPercolated = false;
+            int counter = 0;
+            do {
 
-        var row = StdRandom.uniform(1, n + 1);
-        var column = StdRandom.uniform(1, n + 1);
-        if (!percolation.isOpen(row, column)) {
-          percolation.open(row, column);
-          if (percolation.percolates())
-            isPercolated = true;
+                int row = StdRandom.uniform(1, n + 1);
+                int column = StdRandom.uniform(1, n + 1);
+                if (!percolation.isOpen(row, column)) {
+                    percolation.open(row, column);
+                    if (percolation.percolates())
+                        isPercolated = true;
 
-          counter++;
+                    counter++;
+                }
+            } while (!isPercolated);
+            _thresholds[i] = (double) counter / (n * n);
         }
-      } while (!isPercolated);
-      _thresholds[i] = (double) counter / (n * n);
     }
-  }
 
-  /**
-   * Calculates sample mean of percolation threshold
-   *
-   * @return sample mean
-   */
-  public double mean() {
-    return StdStats.mean(_thresholds);
-  }
+    /**
+     * Calculates sample mean of percolation threshold
+     *
+     * @return sample mean
+     */
+    public double mean() {
+        return StdStats.mean(_thresholds);
+    }
 
-  /**
-   * Calculates sample standard deviation of percolation threshold
-   *
-   * @return sample standard deviation of percolation threshold
-   */
-  public double stddev() {
-    return StdStats.stddev(_thresholds);
-  }
+    /**
+     * Calculates sample standard deviation of percolation threshold
+     *
+     * @return sample standard deviation of percolation threshold
+     */
+    public double stddev() {
+        return StdStats.stddev(_thresholds);
+    }
 
-  /**
-   * Calculates low endpoint of 95% confidence interval
-   *
-   * @return low endpoint of 95% confidence interval
-   */
-  public double confidenceLo() {
-    return mean() - ((1.96 * stddev()) / Math.sqrt(_trials));
-  }
+    /**
+     * Calculates low endpoint of 95% confidence interval
+     *
+     * @return low endpoint of 95% confidence interval
+     */
+    public double confidenceLo() {
+        return mean() - ((1.96 * stddev()) / Math.sqrt(_trials));
+    }
 
-  /**
-   * Calculates high endpoint of 95% confidence interval
-   *
-   * @return high endpoint of 95% confidence interval
-   */
-  public double confidenceHi() {
-    return mean() + ((1.96 * stddev()) / Math.sqrt(_trials));
-  }
+    /**
+     * Calculates high endpoint of 95% confidence interval
+     *
+     * @return high endpoint of 95% confidence interval
+     */
+    public double confidenceHi() {
+        return mean() + ((1.96 * stddev()) / Math.sqrt(_trials));
+    }
 
-  /**
-   * @param args n - number of columns and rows
-   *             T - count of experiments
-   */
-  public static void main(String[] args) {
-    int n = Integer.parseInt(args[0]);
-    int trials = Integer.parseInt(args[1]);
-    var stats = new PercolationStats(n, trials);
-    System.out.println("mean                    = " + stats.mean());
-    System.out.println("stddev                  = " + stats.stddev());
-    System.out.println("95% confidence interval = [" + stats.confidenceLo() + "," + stats.confidenceHi() + "]");
-  }
+    /**
+     * @param args n - number of columns and rows T - count of experiments
+     */
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats stats = new PercolationStats(n, trials);
+        System.out.println("mean                    = " + stats.mean());
+        System.out.println("stddev                  = " + stats.stddev());
+        System.out.println("95% confidence interval = [" + stats.confidenceLo() + "," + stats.confidenceHi() + "]");
+    }
 }
