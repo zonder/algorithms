2a73cbdc59ea449dd0ba606e42da4a2586ad9277
zontik.m@gmail.com <zontik.m@gmail.com>
2018-09-25 23:17:03 +0300

    added initial project

---
+++ week2/Deques/Deque.java
@@ -0,0 +1,20 @@
+/* *****************************************************************************
+ *  Name:    Alexander Masalov
+ *  Description:  Program that illustrates Deque data structure work.
+ *
+ *  Written:       09/23/2018
+ *  Last updated:  09/25/2018
+ *
+ *  % javac-algs4 Deque.java
+ *  % java-algs4 Deque
+ *  226/400
+ *
+ **************************************************************************** */
+
+
+public class Deque {
+
+  public static void main(String[] args) {
+    System.out.println("Deque");
+  }
+}
