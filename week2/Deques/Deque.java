f4abbc3de42e08bcbc185535fccde92cabef667b
zontik.m@gmail.com <zontik.m@gmail.com>
2018-09-26 23:54:13 +0300

    added comments

--- week2/Deques/Deque.java
+++ week2/Deques/Deque.java
@@ -11,9 +11,109 @@
  *
  **************************************************************************** */
 
+import java.util.Iterator;
 
-public class Deque {
+public class Deque<Item> implements Iterable<Item> {
+  /**
+   * Constructs an empty deque
+   */
+  public Deque() {
 
+  }
+
+  /**
+   * Checks is the deque empty?
+   *
+   * @return if the deque is empty?
+   */
+  public boolean isEmpty() {
+    return false;
+  }
+
+  /**
+   * Returns the number of items on the deque
+   *
+   * @return he number of items on the deque
+   */
+  public int size() {
+
+  }
+
+  /**
+   * Adds the item to the front
+   *
+   * @param item to add at the top
+   */
+  public void addFirst(Item item) {
+    if (item == null)
+      throw new java.lang.IllegalArgumentException();
+  }
+
+  /**
+   * Adds the item to the end
+   *
+   * @param item to add at the end
+   */
+  public void addLast(Item item) {
+    if (item == null)
+      throw new java.lang.IllegalArgumentException();
+
+  }
+
+  /**
+   * Removes and return the item from the front
+   *
+   * @return removed item
+   */
+  public Item removeFirst() {
+    if (isEmpty())
+      throw new java.util.NoSuchElementException();
+
+    return null;
+  }
+
+  /**
+   * Removes and return the item from the end
+   *
+   * @return removed item
+   */
+  public Item removeLast() {
+    if (isEmpty())
+      throw new java.util.NoSuchElementException();
+
+    return null;
+  }
+
+  /**
+   * Returns an iterator over items in order from front to end
+   *
+   * @return iterator
+   */
+  public Iterator<Item> iterator() {
+    return new Iterator<Item>() {
+      @Override
+      public boolean hasNext() {
+        return false;
+      }
+
+      @Override
+      public void remove() {
+        throw new java.lang.UnsupportedOperationException();
+      }
+
+      @Override
+      public Item next() {
+        // throw new java.util.NoSuchElementException();
+        return null;
+      }
+    };
+  }
+
+  /**
+   * unit testing (optional)
+   *
+   * @param args external arguments
+   */
   public static void main(String[] args) {
     System.out.println("Deque");
   }
