0e661010bc30040d35e8d1408c4dc1e67c6445e9
Alexander Masalov <alexander.masalov@namecheap.com>
2018-10-05 15:57:31 +0300

    added week3 project

---
+++ week3/PatternRecognition/BruteCollinearPoints.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name:    Alexander Masalov
+ *  Description:  Program that recognizes line patterns in a given set of points.
+ *
+ *  Written:       09/23/2018
+ *  Last updated:  09/25/2018
+ *
+ *  % javac-algs4 BruteCollinearPoints.java
+ *  % java-algs4 BruteCollinearPoints
+ *  226/400
+ *
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class BruteCollinearPoints {
+
+    /**
+     * unit testing (optional)
+     *
+     * @param args external arguments
+     */
+    public static void main(String[] args) {
+        StdOut.println("BruteCollinearPoints");
+    }
+}
---
+++ week3/PatternRecognition/FastCollinearPoints.java
@@ -0,0 +1,26 @@
+/* *****************************************************************************
+ *  Name:    Alexander Masalov
+ *  Description:  Program that recognizes line patterns in a given set of points.
+ *
+ *  Written:       09/23/2018
+ *  Last updated:  09/25/2018
+ *
+ *  % javac-algs4 FastCollinearPoints.java
+ *  % java-algs4 FastCollinearPoints
+ *  226/400
+ *
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class FastCollinearPoints {
+
+    /**
+     * unit testing (optional)
+     *
+     * @param args external arguments
+     */
+    public static void main(String[] args) {
+        StdOut.println("FastCollinearPoints");
+    }
+}
