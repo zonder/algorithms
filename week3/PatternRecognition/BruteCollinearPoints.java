e972d985a2dc427f949cfaec3a24d8868bd76e0a
Alexander Masalov <alexander.masalov@namecheap.com>
2018-10-17 18:33:27 +0300

    finalized brute force solutiion

--- week3/PatternRecognition/BruteCollinearPoints.java
+++ week3/PatternRecognition/BruteCollinearPoints.java
@@ -16,110 +16,95 @@ import java.util.Arrays;
 
 public class BruteCollinearPoints {
 
-  private ArrayList<LineSegment> allSegments;
+    private ArrayList<LineSegment> allSegments;
 
-  /**
-   * Finds all line segments containing 4 points
-   *
-   * @param points all points
-   */
-  public BruteCollinearPoints(Point[] points) {
+    /**
+     * Finds all line segments containing 4 points
+     *
+     * @param points all points
+     */
+    public BruteCollinearPoints(Point[] points) {
 
-    if (points == null)
-      throw new java.lang.IllegalArgumentException("Null points were passed");
+        if (points == null)
+            throw new java.lang.IllegalArgumentException("Null points were passed");
 
-    Arrays.sort(points);
+        Arrays.sort(points);
 
-    allSegments = new ArrayList<>();
-    boolean[] used = new boolean[points.length];
+        allSegments = new ArrayList<>();
 
-    for (int i = 0; i < points.length; i++) {
-      if (points[i] == null)
-        throw new java.lang.IllegalArgumentException("Null point exist");
+        double prevSlope = Double.MIN_VALUE;
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null)
+                throw new java.lang.IllegalArgumentException("Null point exist");
 
-      if (used[i])
-        continue;
+            Point p = points[i];
+            for (int n = i + 1; n < points.length; n++) {
 
-      Point p = points[i];
-      for (int n = i + 1; n < points.length; n++) {
+                boolean lineProcessed = false;
+                if (p.compareTo(points[n]) == 0)
+                    throw new java.lang.IllegalArgumentException("Duplicated point");
 
-        if (used[n])
-          continue;
-        boolean isFound = false;
+                double slope1 = p.slopeTo(points[n]);
+                if (slope1 == prevSlope)
+                    continue;
 
-        if (p.compareTo(points[n]) == 0)
-          throw new java.lang.IllegalArgumentException("Duplicated point");
+                prevSlope = slope1;
 
-        double slope1 = p.slopeTo(points[n]);
+                for (int j = n + 1; j < points.length; j++) {
 
-        for (int j = n + 1; j < points.length; j++) {
+                    if (points[n].compareTo(points[j]) == 0)
+                        throw new java.lang.IllegalArgumentException("Duplicated point");
+                    double slope2 = p.slopeTo(points[j]);
+                    if (slope2 != slope1)
+                        continue;
 
-          if (used[j])
-            continue;
+                    for (int z = points.length - 1; z > j; z--) {
 
-          if (points[n].compareTo(points[j]) == 0)
-            throw new java.lang.IllegalArgumentException("Duplicated point");
-          double slope2 = p.slopeTo(points[j]);
-          if (slope2 != slope1)
-            continue;
+                        if (points[j].compareTo(points[z]) == 0)
+                            throw new java.lang.IllegalArgumentException("Duplicated point");
 
-          for (int z = points.length - 1; z > j; z--) {
+                        if (points[z] == null)
+                            throw new java.lang.IllegalArgumentException("Null point exist");
 
-            if (used[z])
-              continue;
-
-            if (points[j].compareTo(points[z]) == 0)
-              throw new java.lang.IllegalArgumentException("Duplicated point");
-
-            if (points[z] == null)
-              throw new java.lang.IllegalArgumentException("Null point exist");
-
-            double slope3 = p.slopeTo(points[z]);
-            if (slope1 == slope2 && slope2 == slope3) {
-              used[i] = true;
-              used[z] = true;
-
-              allSegments.add(new LineSegment(points[i], points[z]));
-
-              isFound = true;
-              break;
+                        double slope3 = p.slopeTo(points[z]);
+                        if (slope1 == slope2 && slope2 == slope3) {
+                            allSegments.add(new LineSegment(points[i], points[z]));
+                            lineProcessed = true;
+                            break;
+                        }
+                    }
+                    if (lineProcessed)
+                        break;
+                }
             }
-          }
-          if (isFound)
-            break;
         }
-
-        if (isFound)
-          break;
-      }
     }
-  }
 
-  /**
-   * Returns the number of line segments
-   *
-   * @return the number of line segments
-   */
-  public int numberOfSegments() {
-    return allSegments.size();
-  }
+    /**
+     * Returns the number of line segments
+     *
+     * @return the number of line segments
+     */
+    public int numberOfSegments() {
+        return allSegments.size();
+    }
 
-  /**
-   * Returns the line segments
-   *
-   * @return the line segments
-   */
-  public LineSegment[] segments() {
-    LineSegment[] segmentsResult = new LineSegment[allSegments.size()];
-    return allSegments.toArray(segmentsResult);
-  }
+    /**
+     * Returns the line segments
+     *
+     * @return the line segments
+     */
+    public LineSegment[] segments() {
+        LineSegment[] segmentsResult = new LineSegment[allSegments.size()];
+        return allSegments.toArray(segmentsResult);
+    }
 
-  /**
-   * unit testing (optional)
-   *
-   * @param args external arguments
-   */
-  public static void main(String[] args) {
-  }
+    /**
+     * unit testing (optional)
+     *
+     * @param args external arguments
+     */
+    public static void main(String[] args) {
+    }
 
 }
--- week3/PatternRecognition/Point.java
+++ week3/PatternRecognition/Point.java
@@ -15,138 +15,139 @@ import java.util.Comparator;
 
 public class Point implements Comparable<Point> {
 
-  private final int x;     // x-coordinate of this point
-  private final int y;     // y-coordinate of this point
+    private final int x;     // x-coordinate of this point
+    private final int y;     // y-coordinate of this point
 
-  /**
-   * Initializes a new point.
-   *
-   * @param x the <em>x</em>-coordinate of the point
-   * @param y the <em>y</em>-coordinate of the point
-   */
-  public Point(int x, int y) {
-    /* DO NOT MODIFY */
-    this.x = x;
-    this.y = y;
-  }
+    /**
+     * Initializes a new point.
+     *
+     * @param x the <em>x</em>-coordinate of the point
+     * @param y the <em>y</em>-coordinate of the point
+     */
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
 
-  /**
-   * Draws this point to standard draw.
-   */
-  public void draw() {
-    /* DO NOT MODIFY */
-    StdDraw.point(x, y);
-  }
+    /**
+     * Draws this point to standard draw.
+     */
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
 
-  /**
-   * Draws the line segment between this point and the specified point
-   * to standard draw.
-   *
-   * @param that the other point
-   */
-  public void drawTo(Point that) {
-    /* DO NOT MODIFY */
-    StdDraw.line(this.x, this.y, that.x, that.y);
-  }
+    /**
+     * Draws the line segment between this point and the specified point
+     * to standard draw.
+     *
+     * @param that the other point
+     */
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
 
-  /**
-   * Returns the slope between this point and the specified point.
-   * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
-   * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
-   * +0.0 if the line segment connecting the two points is horizontal;
-   * Double.POSITIVE_INFINITY if the line segment is vertical;
-   * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
-   *
-   * @param that the other point
-   * @return the slope between this point and the specified point
-   */
-  public double slopeTo(Point that) {
+    /**
+     * Returns the slope between this point and the specified point.
+     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+     * +0.0 if the line segment connecting the two points is horizontal;
+     * Double.POSITIVE_INFINITY if the line segment is vertical;
+     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+     *
+     * @param that the other point
+     * @return the slope between this point and the specified point
+     */
+    public double slopeTo(Point that) {
 
-    int slopeY = that.y - this.y;
-    int slopeX = that.x - this.x;
-    if (this.compareTo(that) == 0)
-      return Double.NEGATIVE_INFINITY;
-    if (slopeX == 0)
-      return Double.POSITIVE_INFINITY;
-    if (slopeY == 0)
-      return 0;
+        int slopeY = that.y - this.y;
+        int slopeX = that.x - this.x;
+        if (this.compareTo(that) == 0)
+            return Double.NEGATIVE_INFINITY;
+        if (slopeX == 0)
+            return Double.POSITIVE_INFINITY;
+        if (slopeY == 0)
+            return 0;
 
-    return (double) slopeY / slopeX;
-  }
+        return (double) slopeY / slopeX;
+    }
 
-  /**
-   * Compares two points by y-coordinate, breaking ties by x-coordinate.
-   * Formally, the invoking point (x0, y0) is less than the argument point
-   * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
-   *
-   * @param that the other point
-   * @return the value <tt>0</tt> if this point is equal to the argument
-   * point (x0 = x1 and y0 = y1);
-   * a negative integer if this point is less than the argument
-   * point; and a positive integer if this point is greater than the
-   * argument point
-   */
-  public int compareTo(Point that) {
-    if (this.y == that.y && this.x == that.x)
-      return 0;
+    /**
+     * Compares two points by y-coordinate, breaking ties by x-coordinate.
+     * Formally, the invoking point (x0, y0) is less than the argument point
+     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
+     *
+     * @param that the other point
+     * @return the value <tt>0</tt> if this point is equal to the argument
+     * point (x0 = x1 and y0 = y1);
+     * a negative integer if this point is less than the argument
+     * point; and a positive integer if this point is greater than the
+     * argument point
+     */
+    public int compareTo(Point that) {
+        if (this.y == that.y && this.x == that.x)
+            return 0;
 
-    if (this.y < that.y)
-      return -1;
-    else if (this.y == that.y && this.x < that.x)
-      return -1;
-    else
-      return 1;
-  }
+        if (this.y < that.y)
+            return -1;
 
-  /**
-   * Compares two points by the slope they make with this point.
-   * The slope is defined as in the slopeTo() method.
-   *
-   * @return the Comparator that defines this ordering on points
-   */
-  public Comparator<Point> slopeOrder() {
-    return (o1, o2) -> {
+        else if (this.y == that.y && this.x < that.x)
+            return -1;
+        else
+            return 1;
+    }
 
-      double slope1 = o1.slopeTo(o2);
-      double slope2 = o2.slopeTo(o1);
+    /**
+     * Compares two points by the slope they make with this point.
+     * The slope is defined as in the slopeTo() method.
+     *
+     * @return the Comparator that defines this ordering on points
+     */
+    public Comparator<Point> slopeOrder() {
+        return (o1, o2) -> {
 
-      return Double.compare(slope1, slope2);
-    };
-  }
+            double slope1 = o1.slopeTo(o2);
+            double slope2 = o2.slopeTo(o1);
+
+            return Double.compare(slope1, slope2);
+        };
+    }
 
 
-  /**
-   * Returns a string representation of this point.
-   * This method is provide for debugging;
-   * your program should not rely on the format of the string representation.
-   *
-   * @return a string representation of this point
-   */
-  public String toString() {
-    /* DO NOT MODIFY */
-    return "(" + x + ", " + y + ")";
-  }
+    /**
+     * Returns a string representation of this point.
+     * This method is provide for debugging;
+     * your program should not rely on the format of the string representation.
+     *
+     * @return a string representation of this point
+     */
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
 
-  /**
-   * Unit tests the Point data type.
-   */
-  public static void main(String[] args) {
+    /**
+     * Unit tests the Point data type.
+     */
+    public static void main(String[] args) {
 
-    Point[] pArray = new Point[]{
-            new Point(4096, 20992),
-            new Point(5120, 20992),
-            new Point(6144, 20992),
-            new Point(7168, 20992),
-            new Point(8128, 20992),
-            new Point(4096, 22016)};
+        Point[] pArray = new Point[]{
+                new Point(4096, 20992),
+                new Point(5120, 20992),
+                new Point(6144, 20992),
+                new Point(7168, 20992),
+                new Point(8128, 20992),
+                new Point(4096, 22016)};
 
-    StdOut.println();
-    StdOut.println(pArray[0].slopeTo(pArray[1]));
-    StdOut.println(pArray[0].slopeTo(pArray[2]));
-    StdOut.println(pArray[0].slopeTo(pArray[3]));
-    StdOut.println(pArray[0].slopeTo(pArray[4]));
-    StdOut.println(pArray[0].slopeTo(pArray[5]));
-    StdOut.println();
+        StdOut.println();
+        StdOut.println(pArray[0].slopeTo(pArray[1]));
+        StdOut.println(pArray[0].slopeTo(pArray[2]));
+        StdOut.println(pArray[0].slopeTo(pArray[3]));
+        StdOut.println(pArray[0].slopeTo(pArray[4]));
+        StdOut.println(pArray[0].slopeTo(pArray[5]));
+        StdOut.println();
 
-  }
+    }
 }
