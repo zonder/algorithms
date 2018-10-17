311e09cdb3238c3c7add643e50982ac90d6676d5
zontik.m@gmail.com <zontik.m@gmail.com>
2018-10-17 23:41:36 +0300

    fixed fast solution

--- week3/PatternRecognition/BruteCollinearPoints.java
+++ week3/PatternRecognition/BruteCollinearPoints.java
@@ -16,95 +16,99 @@ import java.util.Arrays;
 
 public class BruteCollinearPoints {
 
-    private final ArrayList<LineSegment> allSegments;
+  private final ArrayList<LineSegment> allSegments;
 
-    /**
-     * Finds all line segments containing 4 points
-     *
-     * @param points all points
-     */
-    public BruteCollinearPoints(Point[] points) {
+  /**
+   * Finds all line segments containing 4 points
+   *
+   * @param points all points
+   */
+  public BruteCollinearPoints(Point[] points) {
 
-        if (points == null)
-            throw new java.lang.IllegalArgumentException("Null points were passed");
+    if (points == null)
+      throw new java.lang.IllegalArgumentException("Null points were passed");
 
-        Arrays.sort(points);
+    for (int x = 0; x < points.length; x++) {
+      if (points[x] == null)
+        throw new java.lang.IllegalArgumentException("Null points were passed");
+    }
 
-        allSegments = new ArrayList<>();
+    Point[] pt = points.clone();
 
-        double prevSlope = Double.MIN_VALUE;
-        for (int i = 0; i < points.length; i++) {
-            if (points[i] == null)
-                throw new java.lang.IllegalArgumentException("Null point exist");
 
-            Point p = points[i];
-            for (int n = i + 1; n < points.length; n++) {
+    Arrays.sort(pt);
 
-                boolean lineProcessed = false;
-                if (p.compareTo(points[n]) == 0)
-                    throw new java.lang.IllegalArgumentException("Duplicated point");
+    allSegments = new ArrayList<>();
 
-                double slope1 = p.slopeTo(points[n]);
-                if (slope1 == prevSlope)
-                    continue;
+    double prevSlope = Double.NEGATIVE_INFINITY;
+    for (int i = 0; i < pt.length; i++) {
+      if (pt[i] == null)
+        throw new java.lang.IllegalArgumentException("Null point exist");
 
-                prevSlope = slope1;
+      Point p = pt[i];
+      for (int n = i + 1; n < pt.length; n++) {
+        if (pt[n] == null)
+          throw new java.lang.IllegalArgumentException("Null point exist");
 
-                for (int j = n + 1; j < points.length; j++) {
+        boolean lineProcessed = false;
+        if (p.compareTo(pt[n]) == 0)
+          throw new java.lang.IllegalArgumentException("Duplicated point");
 
-                    if (points[n].compareTo(points[j]) == 0)
-                        throw new java.lang.IllegalArgumentException("Duplicated point");
-                    double slope2 = p.slopeTo(points[j]);
-                    if (slope2 != slope1)
-                        continue;
+        double slope1 = p.slopeTo(pt[n]);
+        if (Double.compare(slope1, prevSlope) == 0)
+          continue;
 
-                    for (int z = points.length - 1; z > j; z--) {
+        prevSlope = slope1;
 
-                        if (points[j].compareTo(points[z]) == 0)
-                            throw new java.lang.IllegalArgumentException("Duplicated point");
+        for (int j = n + 1; j < pt.length; j++) {
 
-                        if (points[z] == null)
-                            throw new java.lang.IllegalArgumentException("Null point exist");
+          if (pt[j] == null)
+            throw new java.lang.IllegalArgumentException("Null point exist");
 
-                        double slope3 = p.slopeTo(points[z]);
-                        if (slope1 == slope2 && slope2 == slope3) {
-                            allSegments.add(new LineSegment(points[i], points[z]));
-                            lineProcessed = true;
-                            break;
-                        }
-                    }
-                    if (lineProcessed)
-                        break;
-                }
+          if (pt[n].compareTo(pt[j]) == 0)
+            throw new java.lang.IllegalArgumentException("Duplicated point");
+          double slope2 = p.slopeTo(pt[j]);
+          if (slope2 != slope1)
+            continue;
+
+          for (int z = pt.length - 1; z > j; z--) {
+
+            if (pt[j].compareTo(pt[z]) == 0)
+              throw new java.lang.IllegalArgumentException("Duplicated point");
+
+            if (pt[z] == null)
+              throw new java.lang.IllegalArgumentException("Null point exist");
+
+            double slope3 = p.slopeTo(pt[z]);
+            if (slope1 == slope2 && slope2 == slope3) {
+              allSegments.add(new LineSegment(pt[i], pt[z]));
+              lineProcessed = true;
+              break;
             }
+          }
+          if (lineProcessed)
+            break;
         }
+      }
     }
+  }
 
-    /**
-     * Returns the number of line segments
-     *
-     * @return the number of line segments
-     */
-    public int numberOfSegments() {
-        return allSegments.size();
-    }
+  /**
+   * Returns the number of line segments
+   *
+   * @return the number of line segments
+   */
+  public int numberOfSegments() {
+    return allSegments.size();
+  }
 
-    /**
-     * Returns the line segments
-     *
-     * @return the line segments
-     */
-    public LineSegment[] segments() {
-        LineSegment[] segmentsResult = new LineSegment[allSegments.size()];
-        return allSegments.toArray(segmentsResult);
-    }
-
-    /**
-     * unit testing (optional)
-     *
-     * @param args external arguments
-     */
-    public static void main(String[] args) {
-    }
-
+  /**
+   * Returns the line segments
+   *
+   * @return the line segments
+   */
+  public LineSegment[] segments() {
+    LineSegment[] segmentsResult = new LineSegment[allSegments.size()];
+    return allSegments.toArray(segmentsResult);
+  }
 }
--- week3/PatternRecognition/FastCollinearPoints.java
+++ week3/PatternRecognition/FastCollinearPoints.java
@@ -18,57 +18,90 @@ import java.util.Arrays;
 
 public class FastCollinearPoints {
 
-    private final ArrayList<LineSegment> allSegments;
+  private final ArrayList<LineSegment> allSegments;
 
-    /**
-     * Finds all line segments containing 4 points
-     *
-     * @param points all points
-     */
-    public FastCollinearPoints(Point[] points) {
-        if (points == null)
-            throw new java.lang.IllegalArgumentException("Null points were passed");
+  /**
+   * Finds all line segments containing 4 points
+   *
+   * @param points all points
+   */
+  public FastCollinearPoints(Point[] points) {
 
-        allSegments = new ArrayList<>();
+    if (points == null)
+      throw new java.lang.IllegalArgumentException("Null points were passed");
 
-        for (int i = 0; i < points.length; i++) {
-            if (points[i] == null)
-                throw new java.lang.IllegalArgumentException("Null point exist");
-
-            Point p = points[i];
-            Arrays.sort(points, i, points.length - 1, p.slopeOrder());
-            for (int n = i)
-
-
-        }
+    for (int x = 0; x < points.length; x++) {
+      if (points[x] == null)
+        throw new java.lang.IllegalArgumentException("Null points were passed");
     }
+    Point[] original = points.clone();
 
 
-    /**
-     * Returns the number of line segments
-     *
-     * @return the number of line segments
-     */
-    public int numberOfSegments() {
-        return 0;
+    Point[] pt = original.clone();
+    //ArrayList<Integer>[] checks = new ArrayList<Integer>[] ();
+
+    allSegments = new ArrayList<>();
+    for (int i = 0; i < pt.length; i++) {
+      if (pt[i] == null)
+        throw new java.lang.IllegalArgumentException("Null point exist");
+
+      Point p = pt[i];
+      Arrays.sort(original, p.slopeOrder());
+
+      int countInRow = 1;
+      double prevSlope = p.slopeTo(original[1]);
+      if (p.compareTo(original[1]) == 0)
+        throw new java.lang.IllegalArgumentException("Duplicated item");
+
+      for (int n = 2; n < original.length; n++) {
+
+        if (p.compareTo(original[n]) == 0)
+          throw new java.lang.IllegalArgumentException("Duplicated item");
+
+        double slope = p.slopeTo(original[n]);
+        if (slope != prevSlope || n == original.length - 1) {
+
+          if (countInRow >= 3) {
+            allSegments.add(new LineSegment(p, original[n - 1]));
+          }
+
+          countInRow = 1;
+        } else
+          countInRow++;
+
+        prevSlope = slope;
+      }
+
     }
-
-    /**
-     * Returns the line segments
-     *
-     * @return the line segments
-     */
-    public LineSegment[] segments() {
-        return null;
-    }
+  }
 
 
-    /**
-     * unit testing (optional)
-     *
-     * @param args external arguments
-     */
-    public static void main(String[] args) {
-        StdOut.println("FastCollinearPoints");
-    }
+  /**
+   * Returns the number of line segments
+   *
+   * @return the number of line segments
+   */
+  public int numberOfSegments() {
+    return allSegments.size();
+  }
+
+  /**
+   * Returns the line segments
+   *
+   * @return the line segments
+   */
+  public LineSegment[] segments() {
+    LineSegment[] segmentsResult = new LineSegment[allSegments.size()];
+    return allSegments.toArray(segmentsResult);
+  }
+
+
+  /**
+   * unit testing (optional)
+   *
+   * @param args external arguments
+   */
+  public static void main(String[] args) {
+    StdOut.println("FastCollinearPoints");
+  }
 }
